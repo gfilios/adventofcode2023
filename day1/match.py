7548a55236bbb65a4a37a472c4ec47043f238a32
gfilios <gator@filios.de>
2024-01-23 15:10:06 +0100

    Working, but wrong sum (to low)

--- day1/match.py
+++ day1/match.py
@@ -1,13 +1,18 @@
 import re
 import sys
 
-p = re.compile('^[a-zA-Z]*([0-9])+')
+firstDigit = re.compile('^[a-zA-Z]*([0-9]{1})[a-zA-Z0-9]*')
+lastDigit = re.compile('^[a-zA-Z0-9]*([0-9]{1})[a-zA-Z]*')
 
-
+sum = 0
 with open('input.txt') as f:
     line=f.readline()
     while line:
       lineclean = line.strip('\n')
-      m = p.match(lineclean)
-      print(f'{lineclean}, {m.group(1 )}')
+      firstMatch = firstDigit.match(lineclean)
+      secondMatch = lastDigit.match(lineclean)
+      print(f'{lineclean}, {firstMatch.group(1)}, {secondMatch.group(1)}')
       line=f.readline()
+      sum = sum + int(firstMatch.group(1)) + int(secondMatch.group(1))
+
+print(sum)
