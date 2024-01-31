12250daa7a1cc5375448d207ec2b10e688d05dde
Georg@Atlas <gthb@filios.de>
2024-01-31 21:07:38 +0100

    Day 3 - Part 1 - Solved

--- day3/day3.py
+++ day3/day3.py
@@ -1,37 +1,40 @@
 import re
 
 number_regex = re.compile('\d+')
+dots_regex = re.compile('\.')
+digit_regex = re.compile('\d')
+
+plan = open('input.txt').read().splitlines()
+line_count = len(plan)
+line_len = len(plan[0])
 
 
 def valid_part(match, plan, lno):
-    span = match.span()
-    span0 = match.span()[0] - 1
-    span1 = match.span()[1] + 1
-    if span0 < 0:
-        span0 = 0
-    if span1 > len(plan[lno]):
-        span1 = len(plan[lno])
+    from_col = max(0, match.span()[0] - 1)
+    to_col = min(match.span()[1] + 1, line_len)
+    from_line = max(0, lno - 1)
+    to_line = min(line_count-1, lno + 1)
 
-    corona = ""
-    if (lno > 0):
-        corona = corona + plan[lno - 1][span0:span1]
-    corona = corona + plan[lno][span0]
-    if (lno < len(plan)):
-        corona = corona + plan[lno + 1][span0:span1]
+    number_chars = to_col-from_col
+    for line in range(from_line, to_line+1):
+        line_part = plan[line][from_col:to_col]
+        dots = len(dots_regex.findall(line_part))
+        digits = len(digit_regex.findall(line_part))
+        if (dots+digits)<number_chars:
+            #print(f"Match {lno}: {match.group()}")
+            return int(match.group())
 
+    return 0
 
 def identify_numbers(plan):
     lno = 0
     sum = 0
     for line in plan:
-        matches = number_regex.findall(line)
-        for match in matches:
+        for match in number_regex.finditer(line):
             sum += valid_part(match, plan, lno)
         lno += 1
     return sum
 
 
 if __name__ == '__main__':
-    f = open('input.txt', 'r')
-    plan = f.readlines()
-    identify_numbers(plan)
+    print(identify_numbers(plan))
